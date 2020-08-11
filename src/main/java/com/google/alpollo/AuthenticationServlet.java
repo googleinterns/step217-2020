a4bf599ef18df7111cb8232563dea565e530834e
Alexandra Ivanova <sashapff@google.com>
2020-08-11 15:52:36 +0300

    Remove unnecessary files

--- src/main/java/com/google/alpollo/AuthenticationServlet.java
+++ src/main/java/com/google/alpollo/AuthenticationServlet.java
@@ -1,4 +1,16 @@
 package com.google.alpollo;
 
-public class AuthenticationServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.google.appengine.api.users.UserService;
+
+@WebServlet("/auth")
+public class AuthenticationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+    }
 }
