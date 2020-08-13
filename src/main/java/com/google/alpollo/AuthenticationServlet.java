4cad3d9445ba6bcd2d5755f6805472dd617e954f
Alexandra Ivanova <sashapff@google.com>
2020-08-13 20:07:15 +0300

    Union status whether user logged in and a link in one json response

--- src/main/java/com/google/alpollo/AuthenticationServlet.java
+++ src/main/java/com/google/alpollo/AuthenticationServlet.java
@@ -2,20 +2,34 @@ package com.google.alpollo;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import com.google.gson.Gson;
 
 @WebServlet("/auth")
 public class AuthenticationServlet extends HttpServlet {
+    private final Gson gson = new Gson();
+
+    private class LoggedInInfo {
+        final boolean loggedIn;
+        final String loginUrl;
+
+        public LoggedInInfo(boolean loggedIn, String loginUrl) {
+            this.loggedIn = loggedIn;
+            this.loginUrl = loginUrl;
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
         UserService userService = UserServiceFactory.getUserService();
+
+        boolean loggedIn = userService.isUserLoggedIn();
         String loginUrl = userService.createLoginURL("/");
-        // Redirect to the login page and then to the main page
-        response.sendRedirect(loginUrl);
+        response.getWriter().println(gson.toJson(new LoggedInInfo(loggedIn, loginUrl)));
     }
 }
