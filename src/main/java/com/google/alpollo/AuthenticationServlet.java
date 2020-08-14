93a819fd896817caa33e32d2a7350b73d75144c1
Alexandra Ivanova <sashapff@google.com>
2020-08-14 13:59:29 +0300

    Add the documentation to auth servlet

--- src/main/java/com/google/alpollo/AuthenticationServlet.java
+++ src/main/java/com/google/alpollo/AuthenticationServlet.java
@@ -9,10 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import com.google.gson.Gson;
 
+/** Servlet that returns an auth info that includes status whether the user logged in and link to auth. */
 @WebServlet("/auth")
 public class AuthenticationServlet extends HttpServlet {
     private final Gson gson = new Gson();
 
+    /** Class provides info about logged in status of the user and link to the authentication. */
     private class LoggedInInfo {
         private final boolean loggedIn;
         private final String authUrl;
