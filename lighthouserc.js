c6834f1bed83809f6cf1f2debbdb031cf5bd3ce5
Olga Shimanskaia <oshimanskaia@google.com>
2020-08-14 20:31:48 +0300

    fixed port in lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
       collect: {
-        url: ['http://localhost:3000/'],
+        url: ['http://localhost:8080/'],
         startServerCommand: 'mvn package appengine:run',
       },
       upload: {
