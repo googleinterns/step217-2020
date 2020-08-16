f26a52d1927f59ea5a0b49d5902de67884a1fa38
Olga Shimanskaia <ol_shiman@mail.ru>
2020-08-16 10:42:35 +0300

    Added asserts and removed pause

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,15 +1,45 @@
 module.exports = {
     extends: 'lighthouse:default',
-    passes: [
-      { passName: 'defaultPass', pauseAfterLoadMs: 25000 }
-    ],
     ci: {
       collect: {
         url: ['http://localhost:8080/'],
         startServerCommand: 'mvn package appengine:run',
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo'
+        ],
+        skipAudits: [
+          'unused-javascript',
+          'uses-text-compression'
+        ],
       },
       upload: {
         target: 'temporary-public-storage'
       },
+      assert: {
+        preset: 'lighthouse:no-pwa',
+        assertions: {      
+          'categories:performance': [
+              'warn',
+              { aggregationMethod: 'optimistic', minScore: 0.95}
+          ],
+          'categories:accessibility': [
+              'warn', 
+              { aggregationMethod: 'optimistic', minScore: 1}
+          ],
+          'categories:best-practices': [
+              'warn',
+              { aggregationMethod: 'optimistic', minScore: 1}
+          ],
+          'categories:seo': [
+              'warn',
+              { aggregationMethod: 'optimistic', minScore: 1}
+          ],
+          'unused-javascript': 'off',
+          'uses-text-compression': 'off'
+        }
+      }
     },
-};
+  };
