5c8abaa47a724d06fd7010b19ff0f8273b32c578
Răzvan Budacă <rbudaca@google.com>
2020-08-17 16:02:35 +0000

    Implement method for analyzing given string and returns the sentiment object

--- src/main/java/com/google/alpollo/SentimentServlet.java
+++ src/main/java/com/google/alpollo/SentimentServlet.java
@@ -6,13 +6,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import com.google.gson.Gson;
 
 @WebServlet("/sentiment")
 public class SentimentServlet extends HttpServlet {
+  private final Gson gson = new Gson();
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String lyrics = request.getParameter("lyrics");
-    
+    Sentiment sentiment = getSentiment(lyrics);
+  }
+
+  /**
+   * Based on the lyrics sent, the AI can extract the main "sentiment" of the text.
+   * This sentiment has a score, showing the overall positivity of the text, ranging from -1 to 1
+   * and a magnitude, representing how strong the sentiment is, ranging from 0 to 1
+   */
+  public Sentiment getSentiment(String lyrics) throws IOException {
+    Document doc =
+        Document.newBuilder().setContent(lyrics).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    languageService.close();
+    return sentiment;
   }
 }
