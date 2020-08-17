482ee0a0212804c461d83cd6ae4be455a38897f1
Răzvan Budacă <rbudaca@google.com>
2020-08-17 11:37:09 +0000

    Comment SongInfo class

--- src/main/java/com/google/alpollo/SongInfo.java
+++ src/main/java/com/google/alpollo/SongInfo.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 import com.google.cloud.language.v1.Entity;
 
+/**
+ * This class will be used to store all the results of the analysis.
+ * For now, it's just gonna store the name, score and entityList resulted from the analysis
+ * for easier and more elegant use.
+ * In the future it could include albumName, genre, lyrics.
+ */
+
 public class SongInfo {
   private String songName;
   private double score;
