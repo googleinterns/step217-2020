07758a288e8dda7a747898b65fb0c6be0fb4c6db
Răzvan Budacă <rbudaca@google.com>
2020-08-17 11:17:02 +0000

    Add SongInfo class with constructor for analysis results storage

---
+++ src/main/java/com/google/alpollo/SongInfo.java
@@ -0,0 +1,18 @@
+package com.google.alpollo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.cloud.language.v1.Entity;
+
+public class SongInfo {
+    private String songName;
+    private int score;
+    private List<Entity> entityList = new ArrayList<>();
+
+    public SongInfo(String songName, int score, List<Entity> entityList) {
+        this.songName = songName;
+        this.score = score;
+        this.entityList = entityList;
+    }   
+}
