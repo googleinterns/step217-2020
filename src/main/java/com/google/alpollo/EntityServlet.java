93278b908207d27ae3869c8f87dcf9b21ccf9f2d
Răzvan Budacă <rbudaca@google.com>
2020-08-19 12:28:36 +0000

    Create EntityServlet for POST request

---
+++ src/main/java/com/google/alpollo/EntityServlet.java
@@ -0,0 +1,21 @@
+package com.google.alpollo;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@WebServlet("/entity")
+public class EntityServlet extends HttpServlet {
+  private final Gson gson = new Gson();
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    
+  }
+}
