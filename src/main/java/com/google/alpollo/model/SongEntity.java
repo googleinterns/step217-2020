b7a33eda3d41433dc9845739c02116e68036a31e
Răzvan Budacă <rbudaca@google.com>
2020-08-19 14:43:19 +0000

    Add a comparator that sorts song entities by salience

--- src/main/java/com/google/alpollo/model/SongEntity.java
+++ src/main/java/com/google/alpollo/model/SongEntity.java
@@ -1,5 +1,7 @@
 package com.google.alpollo.model;
 
+import java.util.Comparator;
+
 /**
  * Class created to store only entity name and salience, leaving the other useless properties
  * of the Entity object.
@@ -13,4 +15,14 @@ public class SongEntity {
     this.name = name;
     this.salience = salience;
   }
+
+  /**
+   * A comparator for sorting song entities by their start time in ascending order.
+   */
+  public static final Comparator<SongEntity> ORDER_BY_SALIENCE = new Comparator<SongEntity>() {
+    @Override
+    public int compare(SongEntity a, SongEntity b) {
+      return Float.compare(a.salience, b.salience);
+    }
+  };
 }
