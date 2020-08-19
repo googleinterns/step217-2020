bdf5c4467405e08352b16a8716222fd989a55e34
Răzvan Budacă <rbudaca@google.com>
2020-08-19 12:59:21 +0000

    Grab the entityList using our helper

--- src/main/java/com/google/alpollo/EntityServlet.java
+++ src/main/java/com/google/alpollo/EntityServlet.java
@@ -1,12 +1,15 @@
 package com.google.alpollo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.language.v1.Entity;
 import com.google.gson.Gson;
 
 @WebServlet("/entity")
@@ -17,6 +20,6 @@ public class EntityServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String lyrics = request.getParameter("lyrics");
     AnalysisHelper helper = new AnalysisHelper();
-    
+    List<Entity> entityList = new ArrayList<>(helper.getEntityList(lyrics));
   }
 }
