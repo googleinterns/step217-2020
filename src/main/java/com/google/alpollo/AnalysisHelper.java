723c98a8a5d2a94d92e088edfdb2e4cc16e86481
Olga Shimanskaia <oshimanskaia@google.com>
2020-08-20 12:36:05 +0300

    Removed unused variable

--- src/main/java/com/google/alpollo/AnalysisHelper.java
+++ src/main/java/com/google/alpollo/AnalysisHelper.java
@@ -6,7 +6,6 @@ import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.LanguageServiceSettings;
 import com.google.cloud.language.v1.Sentiment;
-import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
@@ -15,8 +14,6 @@ import com.google.gson.JsonSyntaxException;
  * from the API
  */
 public class AnalysisHelper {
-  private final Gson gson = new Gson();
-
   /**
    * Based on the lyrics sent, the AI can extract the main "sentiment" of the text.
    * This sentiment has a score, showing the overall positivity of the text, ranging from -1.0 to 1.0
