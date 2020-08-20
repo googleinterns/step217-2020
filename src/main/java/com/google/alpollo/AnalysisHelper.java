33006296246556e2e7a7076ea78f689356f8e7dc
Răzvan Budacă <rbudaca@google.com>
2020-08-20 17:14:45 +0000

    Settings are now static

--- src/main/java/com/google/alpollo/AnalysisHelper.java
+++ src/main/java/com/google/alpollo/AnalysisHelper.java
@@ -12,15 +12,21 @@ import com.google.cloud.language.v1.Sentiment;
  * from the API.
  */
 public class AnalysisHelper {
+  private static LanguageServiceSettings SETTINGS;
+
+  private AnalysisHelper() {};
+  
   /**
    * Based on the lyrics sent, the AI can extract the main "sentiment" of the text.
    * This sentiment has a score, showing the overall positivity of the text, ranging from -1.0 to 1.0
    * and a magnitude, representing how strong the sentiment is, ranging from 0.0 to +inf.
    */
   public static Sentiment getSentiment(String lyrics) throws IOException {
-    LanguageServiceSettings settings = LanguageServiceSettings.newBuilder().setHeaderProvider(
-        FixedHeaderProvider.create("X-Goog-User-Project", "google.com:alpollo-step-2020")).build();
-    try (LanguageServiceClient language = LanguageServiceClient.create(settings)) {
+    if (SETTINGS == null) {
+      SETTINGS = LanguageServiceSettings.newBuilder().setHeaderProvider(
+          FixedHeaderProvider.create("X-Goog-User-Project", "google.com:alpollo-step-2020")).build();
+    }
+    try (LanguageServiceClient language = LanguageServiceClient.create(SETTINGS)) {
       Document doc = Document.newBuilder().setContent(lyrics).setType(Document.Type.PLAIN_TEXT).build();
       Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
       return sentiment;
