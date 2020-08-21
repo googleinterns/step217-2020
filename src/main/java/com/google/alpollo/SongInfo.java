c45a3ca2dc66694cc5886b49c856471e8eba0f60
Răzvan Budacă <rbudaca@google.com>
2020-08-21 11:39:56 +0000

    Let's move those comments

--- src/main/java/com/google/alpollo/SongInfo.java
+++ src/main/java/com/google/alpollo/SongInfo.java
@@ -6,14 +6,13 @@ import com.google.alpollo.model.SongEntity;
 /**
  * Class used for storing data related to the Natural Language API analysis.
  * Extending the Song class, it provides all the info related to a specific song.
- * 
- * Score tells us the overall positivty of the song, ranging from -1.0 to 1.0.
- * Magnitude tells us how strong the main sentiment of the song is, ranging from 0.0 to +inf.
- * TopSalientEntities is a list containing the 10 most important words, given the song context.
  */
 public class SongInfo extends Song{
+  /** Score tells us the overall positivty of the song, ranging from -1.0 to 1.0.*/
   private final float score;
+  /** Magnitude tells us how strong the main sentiment of the song is, ranging from 0.0 to +inf. */
   private final float magnitude;
+  /** TopSalientEntities is a list containing the 10 most important words, given the song context.*/
   private final List<SongEntity> topSalientEntities;
 
   public SongInfo(String artist, String name, String album, float score, float magnitude,
