0acc043585f18f1d63399f7cf72df17bbb1429e1
Răzvan Budacă <rbudaca@google.com>
2020-08-21 11:41:42 +0000

    Spaces are my enemy

--- src/main/java/com/google/alpollo/SongInfo.java
+++ src/main/java/com/google/alpollo/SongInfo.java
@@ -7,7 +7,7 @@ import com.google.alpollo.model.SongEntity;
  * Class used for storing data related to the Natural Language API analysis.
  * Extending the Song class, it provides all the info related to a specific song.
  */
-public class SongInfo extends Song{
+public class SongInfo extends Song {
   /** Score tells us the overall positivty of the song, ranging from -1.0 to 1.0.*/
   private final float score;
   /** Magnitude tells us how strong the main sentiment of the song is, ranging from 0.0 to +inf. */
