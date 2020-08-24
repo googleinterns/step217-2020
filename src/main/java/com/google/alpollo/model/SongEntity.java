3902334a19b6572ce5f4b2438a1fe2c7b9a01371
Răzvan Budacă <rbudaca@google.com>
2020-08-24 12:22:16 +0000

    Add type and metadata to SongEntity class

--- src/main/java/com/google/alpollo/model/SongEntity.java
+++ src/main/java/com/google/alpollo/model/SongEntity.java
@@ -1,6 +1,7 @@
 package com.google.alpollo.model;
 
 import java.util.Comparator;
+import java.util.HashMap;
 
 /**
  * Class created to store only entity name and salience, leaving the other useless properties
@@ -9,12 +10,23 @@ import java.util.Comparator;
  */
 public class SongEntity {
   private final String name;
-  // Each entity has a salience score, telling us how important the word is in the given context.
+  /**  Each entity has a salience score, telling us how important the word is in the given context. */
   private final double salience;
+  /** 
+   * The API sets each entity to a category.
+   * e.g. PERSON, LOCATION, WORK_OF_ART*/ 
+  private final String type;
+  /** 
+   * For most entity types, the metadata is a Wikipedia URL. Other times, it can be extra info.
+   * An object containing a list of "key": value pairs. Example: { "name": "wrench", "mass": "1.3kg", "count": "3" }.
+  */
+  private final HashMap<String, String> metadata;
 
-  public SongEntity(String name, double salience) {
+  public SongEntity(String name, double salience, String type, HashMap<String, String> metadata) {
     this.name = name;
     this.salience = salience;
+    this.type = type;
+    this.metadata = metadata;
   }
 
   /**
