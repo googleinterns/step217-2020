73e6def7d148f518006c712f774b3a19250d60a0
Alexandra <a.ivanova@niuitmo.ru>
2020-08-27 11:28:17 +0000

    Update counter

--- src/main/java/com/google/alpollo/SongDataBase.java
+++ src/main/java/com/google/alpollo/SongDataBase.java
@@ -7,15 +7,21 @@ import java.util.List;
 public class SongDataBase {
   private static final int TOP_SIZE = 10;
 
-  /** Save request song to database. */
+  /** Save request song to database and increase the search counter. */
   public static void saveSongRequest(Song song) {
-    OfyService.ofy().save().entity(new SongHead(song)).now();
+    String id = SongHead.getSongId(song);
+    SongHead songHead = OfyService.ofy().load().type(SongHead.class).id(id).now();
+    if (songHead == null) {
+      songHead = new SongHead(song);
+    }
+    songHead.increaseSearchCounter();
+    OfyService.ofy().save().entity(songHead).now();
   }
 
-  /** Returns the list of at most 10 songs. */
+  /** Returns a list of up to 10 most searched songs. */
   public static List<Song> topSongs() {
     List<Song> songs = new ArrayList<>();
-    OfyService.ofy().load().type(SongHead.class).limit(TOP_SIZE).list()
+    OfyService.ofy().load().type(SongHead.class).order("-searchCounter").limit(TOP_SIZE).list()
         .forEach(song -> songs.add((Song) song));
     return songs;
   }
