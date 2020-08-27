347749a2dc6ca289407c88345f0426174e9ef59f
Alexandra <a.ivanova@niuitmo.ru>
2020-08-27 09:54:57 +0000

    Create the field to counter

--- src/main/java/com/google/alpollo/SongHead.java
+++ src/main/java/com/google/alpollo/SongHead.java
@@ -8,12 +8,17 @@ public class SongHead extends Song {
   @Id
   private String id;
 
+  /** Number of times users searched for this song in the system. */
+  @Index
+  private int searchCounter;
+
   SongHead() { }
 
   public SongHead(Song song) {
     super(song);
     // TODO: add hashcode instead of concatenation
     id = song.getArtist() + song.getName() + song.getAlbum();
+    searchCounter = 1;
   }
 
 }
