af51f0f4da8e8bb2f59c122ac6bea6d4c61e64ff
Olga Shimanskaia <oshimanskaia@google.com>
2020-08-27 21:12:03 +0300

    removed not related changes

--- src/main/java/com/google/alpollo/TopSongsServlet.java
+++ src/main/java/com/google/alpollo/TopSongsServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import com.google.gson.Gson;
 
@@ -20,16 +19,6 @@ public class TopSongsServlet extends HttpServlet {
     response.setContentType("application/json");
 
     final List<Song> songs = SongDataBase.topSongs();
-    /*final List<Song> songs = Arrays.asList(new Song("The Beatles", "Yesterday", ""),
-        new Song("Queen", "Bohemian Rhapsody", ""),
-        new Song("Eagles", "Hotel California", ""),
-        new Song("Little Big", "Go Bananas", ""),
-        new Song("Hurts", "Miracle", ""),
-        new Song("Black Veil Brides", "In The End", ""),
-        new Song("Breaking Benjamin", "Ashes of Eden", ""),
-        new Song("The HU ft. Papa Roach", "Wolf Totem", ""),
-        new Song("The Weeknd", "Blinding Lights", ""),
-        new Song("Panic! At The Disco", "Roaring 20s", ""));*/
     response.getWriter().write(gson.toJson(songs));
   }
 }
