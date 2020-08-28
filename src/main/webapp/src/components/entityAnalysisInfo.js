559db10998ebc840d1f9f0f401598b8f39054dac
Răzvan Budacă <rbudaca@google.com>
2020-08-28 14:59:50 +0000

    Add list of links below the chart

--- src/main/webapp/src/components/entityAnalysisInfo.js
+++ src/main/webapp/src/components/entityAnalysisInfo.js
@@ -4,6 +4,7 @@ import { withStyles } from "@material-ui/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import axios from "axios";
 import Chart from "react-google-charts";
+import Typography from "@material-ui/core/Typography";
 
 const google = window.google;
 
@@ -83,27 +84,45 @@ class EntityAnalysisInfo extends React.Component {
       return <p>No entities were found.</p>;
     }
 
-    /** Gather the most important words in a simple array */
+    /** Gather the most important words and their data in simple arrays */
     var top10WordsData = [];
+    var top10WordsLinks = [];
     top10WordsData.push(['Word and Type', 'Importance'])
     this.state.entityAnalysisInfo.forEach((entity) => {
       top10WordsData.push([entity.name + ' (' + entity.type + ')', entity.salience]);
+      if (entity.wikiLink !== "") {
+        top10WordsLinks.push(entity.wikiLink);
+      }
     })
+
+    const listLinks = top10WordsLinks.map((link, index) =>
+      <li key={index + 1}>
+        <a href={link}> {link} </a>
+      </li>
+    );
      
  
     return (
-      <div style={{ display: 'flex', maxWidth: 900 }}>
-        <Chart
-          width={800}
-          height={400}
-          chartType="PieChart"
-          loader={<div>Loading Chart</div>}
-          data={top10WordsData}
-          options={{
-            title: 'Most Important Words',
-          }}
-          legendToggle
-        />
+      <div>
+        <div style={{ display: 'flex', maxWidth: 900 }}>
+          <Chart
+            width={800}
+            height={400}
+            chartType="PieChart"
+            loader={<div>Loading Chart</div>}
+            data={top10WordsData}
+            options={{
+              title: 'Most Important Words',
+            }}
+            legendToggle
+          />
+        </div>
+        <div>
+        <Typography variant="h5">Wiki links found</Typography>
+          <ul>
+            {listLinks}         
+          </ul>
+        </div>
       </div>
     );
   }
