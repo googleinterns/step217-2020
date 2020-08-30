dc058f05e96294687c01ab11f92f249c7c132e00
Olga Shimanskaia <ol_shiman@mail.ru>
2020-08-30 12:34:14 +0000

    Added proxy and files for deployment

--- src/main/webapp/src/setupProxy.js
+++ src/main/webapp/src/setupProxy.js
@@ -1,6 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const nodejs_port = 5000;
+const backend_port = 8080;
 
 module.exports = function(app) {
     app.use(createProxyMiddleware('/api', {target: `http://127.0.0.1:${nodejs_port}` }));
+    app.use(createProxyMiddleware('/', {target: `http://127.0.0.1:${backend_port}` }));
 };
