26740dd501945d51fa39b4c6104973bec0af9acd
Răzvan Budacă <rbudaca@google.com>
2020-09-01 16:42:43 +0000

    Fix some spacing

--- src/main/java/com/google/alpollo/SongInfoServlet.java
+++ src/main/java/com/google/alpollo/SongInfoServlet.java
@@ -1,12 +1,10 @@
 package com.google.alpollo;
 
 import java.util.List;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.alpollo.model.SongEntity;
 import com.google.alpollo.model.SongInfo;
 import com.google.gson.Gson;
