95c7eb282a39843ce96ddca8072924fe7dadce2a
Răzvan Budacă <rbudaca@google.com>
2020-09-01 15:33:04 +0000

    Comment our doGet() method

--- src/main/java/com/google/alpollo/SongInfoServlet.java
+++ src/main/java/com/google/alpollo/SongInfoServlet.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.alpollo.model.SongInfo;
 import com.google.gson.Gson;
 
+/** Servlet created to deal with songInfo related operations */
 @WebServlet("/song-info")
 public class SongInfoServlet extends HttpServlet {
   private static final String SONG_ID = "songId";
   private final Gson gson = new Gson();
 
+  /** 
+   * Making a GET request to our servlet with the desired song ID as a parameter will return that 
+   * song from the database.
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) {
     String songId = request.getParameter(SONG_ID);
