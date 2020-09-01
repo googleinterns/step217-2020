536ff8105015dd71818e3010ebc46334092fba5e
Alexandra <a.ivanova@niuitmo.ru>
2020-09-01 14:10:00 +0000

    Remove Objectify Filter

--- src/main/java/com/google/alpollo/ObjectifyWebFilter.java
+++
@@ -1,6 +0,0 @@
-import javax.servlet.annotation.WebFilter;
-import com.googlecode.objectify.ObjectifyFilter;
-
-/** Helper class for filtering data in a database. */
-@WebFilter(urlPatterns = {"/*"})
-public class ObjectifyWebFilter extends ObjectifyFilter {}
