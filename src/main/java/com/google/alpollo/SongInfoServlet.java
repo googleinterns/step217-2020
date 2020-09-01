46321660e13979b90cf6b8f727fbf8d646988f06
Răzvan Budacă <rbudaca@google.com>
2020-09-01 15:13:11 +0000

    Send our songInfo object from the database back to client

--- src/main/java/com/google/alpollo/SongInfoServlet.java
+++ src/main/java/com/google/alpollo/SongInfoServlet.java
@@ -4,14 +4,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.alpollo.model.SongInfo;
+import com.google.gson.Gson;
 
 @WebServlet("/song-info")
 public class SongInfoServlet extends HttpServlet {
   private static final String SONG_ID = "songId";
+  private final Gson gson = new Gson();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) {
     String songId = request.getParameter(SONG_ID);
     SongInfo songInfo = SongDataBase.getSongInfo(songId);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(songInfo));
   }
 }
