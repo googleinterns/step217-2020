378f947cf240a30d6f76f27688f0b03c8d2ffbaf
Alexandra <a.ivanova@niuitmo.ru>
2020-09-01 11:57:10 +0000

    Fix documentation

--- src/main/java/com/google/alpollo/OfyService.java
+++ src/main/java/com/google/alpollo/OfyService.java
@@ -6,17 +6,17 @@ import com.googlecode.objectify.ObjectifyService;
 
 /** Helper class to start running Objectify. */
 public class OfyService {
-  /** Register Song class in Objectify. */
+  /** Registers Song class in Objectify. */
   static {
     ObjectifyService.register(Song.class);
   }
 
-  /** Redefinition ofy method. */
+  /** Redefines the ofy method. */
   static Objectify ofy() {
     return ObjectifyService.ofy();
   }
 
-  /** Redefinition factory method. */
+  /** Redefines the factory method. */
   static ObjectifyFactory factory() {
     return ObjectifyService.factory();
   }
