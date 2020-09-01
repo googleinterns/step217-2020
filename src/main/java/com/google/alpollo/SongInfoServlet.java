5c0a29b4b84f513ed3f1491646d99f69933be6a0
Răzvan Budacă <rbudaca@google.com>
2020-09-01 15:01:33 +0000

    Create our new servlet

---
+++ src/main/java/com/google/alpollo/SongInfoServlet.java
@@ -0,0 +1,16 @@
+package com.google.alpollo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/song-info")
+public class SongInfoServlet extends HttpServlet {
+  private static final String SONG_ID = "songId";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    String songId = request.getParameter(SONG_ID);
+  }
+}
