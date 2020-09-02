851efef7eebebb8e5928eed5172787b40b713882
Răzvan Budacă <rbudaca@google.com>
2020-09-02 12:52:51 +0000

    Remove useless import

--- src/main/java/com/google/alpollo/AnalysisHelper.java
+++ src/main/java/com/google/alpollo/AnalysisHelper.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.alpollo.model.SongEntity;
-import com.google.alpollo.model.SongSentiment;
 import com.google.api.gax.rpc.FixedHeaderProvider;
 import com.google.cloud.language.v1.AnalyzeEntitiesRequest;
 import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
