870926c84a7e382ad551f215820dd314388ab3a5
Alexandra <a.ivanova@niuitmo.ru>
2020-09-02 11:07:53 +0000

    Add methods to save and to get song info

--- src/main/java/com/google/alpollo/SongDataBase.java
+++ src/main/java/com/google/alpollo/SongDataBase.java
@@ -1,6 +1,7 @@
 package com.google.alpollo;
 
 import java.util.List;
+import com.google.alpollo.model.SongInfo;
 
 /** Class that provides methods to work with the database. */
 public class SongDataBase {
@@ -15,5 +16,15 @@ public class SongDataBase {
   /** Returns the list of at most 10 songs. */
   public static List<Song> topSongs() {
     return OfyService.ofy().load().type(Song.class).limit(TOP_SIZE).list();
+  }
+
+  /** Save whole song info to database. */
+  public static void saveSongInfo(SongInfo songInfo) {
+    OfyService.ofy().save().entity(songInfo).now();
+  }
+
+  /** Returns the song info by song id. */
+  public static SongInfo getSongInfo(int id) {
+    return OfyService.ofy().load().type(SongInfo.class).id(id).now();
   }
 }
