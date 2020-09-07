67369504d502e40d4e704808ed1c767856492fc8
Răzvan Budacă <rbudaca@google.com>
2020-09-07 14:49:38 +0000

    Change parentSong to song and change doc for youtube ids

--- src/main/java/com/google/alpollo/model/SongInfo.java
+++ src/main/java/com/google/alpollo/model/SongInfo.java
@@ -13,7 +13,7 @@ import com.googlecode.objectify.annotation.Entity;
 @Entity
 public class SongInfo {
   /** All the info in this class is attached to a specific parent song. */
-  private final Song parentSong;
+  private final Song song;
   /** Score tells us the overall positivty of the song, ranging from -1.0 to 1.0. */
   @Index private float score;
   /** Magnitude tells us how strong the main sentiment of the song is, ranging from 0.0 to +inf. */
@@ -21,12 +21,12 @@ public class SongInfo {
   /** TopSalientEntities is a list containing the 10 most important words, given the song context. */
   @Index private List<SongEntity> topSalientEntities;
   @Index private String lyrics;
-  /** After analyzing the entities, we'll have a list of recommended YouTube links. */
+  /** After analyzing the entities, we'll have a list of recommended YouTube video IDs. */
   @Index private List<String> youTubeIds;
 
-  public SongInfo(Song parentSong, float score, float magnitude,
+  public SongInfo(Song song, float score, float magnitude,
       List<SongEntity> topSalientEntities, String lyrics, List<String> youTubeIds) {
-    this.parentSong = parentSong;
+    this.song = song;
     this.score = score;
     this.magnitude = magnitude;
     this.topSalientEntities = topSalientEntities;
