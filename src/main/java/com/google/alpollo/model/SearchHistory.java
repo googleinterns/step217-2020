fe29f63b6db9d9436092f781066044113df1b8b3
Alexandra <a.ivanova@niuitmo.ru>
2020-09-21 15:40:40 +0000

    Add counter to songs

--- src/main/java/com/google/alpollo/model/SearchHistory.java
+++ src/main/java/com/google/alpollo/model/SearchHistory.java
@@ -1,8 +1,10 @@
 package com.google.alpollo.model;
 
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
 import com.googlecode.objectify.annotation.Index;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Entity;
@@ -14,7 +16,9 @@ import com.googlecode.objectify.annotation.Entity;
 @Entity
 public class SearchHistory {
   @Id private String id;
-  @Index private HashSet<Song> history = new HashSet<>();
+  @Index private HashMap<Song, Integer> history = new HashMap<>();
+  /** Number of the songs that will be shown to the user. */
+  private static final int TOP_SIZE = 10;
 
   /** Objectify requires no argument constructor. Do not use it. */
   private SearchHistory() { }
@@ -23,11 +27,24 @@ public class SearchHistory {
     id = userId;
   }
 
+  /** Saves new  search request from autorized user. */
   public void addSearchRequest(Song song) {
-    history.add(song);
+    if (history.containsKey(song)) {
+      history.put(song, history.get(song) + 1);
+    } else {
+      history.put(song, 1);
+    }
   }
 
+  /** Returns list with top 10 searched songs from autorized user. */
   public List<Song> getHistory() {
-    return new ArrayList<>(history);
+    // sort by values and then add only top 10 songs to result list 
+    List<Song> sortedSongs = new ArrayList<>();
+    history.entrySet()
+        .stream()
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .limit(TOP_SIZE)
+        .forEachOrdered(x -> sortedSongs.add(x.getKey()));
+    return sortedSongs;
   }
 }
