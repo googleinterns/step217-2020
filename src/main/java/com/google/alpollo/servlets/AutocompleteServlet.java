d3f6c82fa06729e3fb22dbd61f50a35f911880e2
Răzvan Budacă <rbudaca@google.com>
2020-09-21 16:22:10 +0000

    Config JsonPath with Gson

--- src/main/java/com/google/alpollo/servlets/AutocompleteServlet.java
+++ src/main/java/com/google/alpollo/servlets/AutocompleteServlet.java
@@ -3,7 +3,9 @@ package com.google.alpollo.servlets;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +18,18 @@ import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
+import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.GsonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.GsonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
 
 @WebServlet("/autocomplete")
 public class AutocompleteServlet extends HttpServlet {
@@ -30,15 +37,15 @@ public class AutocompleteServlet extends HttpServlet {
   private final static String SEARCH_STRING = "searchString";
   private final static String TYPE = "type";
   private final static String LIMIT = "10";
+
   private enum SearchType {
-    ARTIST,
-    SONG;
+    ARTIST, SONG;
   };
 
   /**
-   * Making a POST request to this servlet with a search string and the type searched for
-   * as parameters will make a call to the Knowledge Graph Search API and it will return a list of
-   * possible search results.
+   * Making a POST request to this servlet with a search string and the type
+   * searched for as parameters will make a call to the Knowledge Graph Search API
+   * and it will return a list of possible search results.
    */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -60,8 +67,6 @@ public class AutocompleteServlet extends HttpServlet {
       HttpTransport httpTransport = new NetHttpTransport();
       HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
 
-      JSONParser parser = new JSONParser();
-
       GenericUrl url = new GenericUrl("https://kgsearch.googleapis.com/v1/entities:search");
       url.put("query", searchString);
       url.put("limit", LIMIT);
@@ -70,8 +75,9 @@ public class AutocompleteServlet extends HttpServlet {
 
       HttpRequest autocompleteRequest = requestFactory.buildGetRequest(url);
       HttpResponse autocompleteResponse = autocompleteRequest.execute();
-      JSONObject responseObject = (JSONObject) parser.parse(autocompleteResponse.parseAsString());
-      JSONArray elements = (JSONArray) responseObject.get("itemListElement");
+      configJsonPathWithGson();
+      JsonObject responseObject = (JsonObject) JsonParser.parseString(autocompleteResponse.parseAsString());
+      JsonArray elements = (JsonArray) responseObject.get("itemListElement");
 
       List<String> results = new ArrayList<>();
       for (Object element : elements) {
@@ -81,10 +87,32 @@ public class AutocompleteServlet extends HttpServlet {
       String json = gson.toJson(results);
       response.setContentType("application/json");
       response.getWriter().println(json);
-    } catch (JsonSyntaxException | JsonIOException | IOException | ParseException autoException) {
+    } catch (JsonSyntaxException | JsonIOException | IOException autoException) {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, autoException.getMessage());
     } catch (IllegalArgumentException enumException) {
       response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Type not supported.");
     }
+  }
+
+  private void configJsonPathWithGson() {
+    Configuration.setDefaults(new Configuration.Defaults() {
+      private final JsonProvider jsonProvider = new GsonJsonProvider();
+      private final MappingProvider mappingProvider = new GsonMappingProvider();
+        
+      @Override
+      public JsonProvider jsonProvider() {
+          return jsonProvider;
+      }
+  
+      @Override
+      public MappingProvider mappingProvider() {
+          return mappingProvider;
+      }
+      
+      @Override
+      public Set<Option> options() {
+          return EnumSet.noneOf(Option.class);
+      }
+    });
   }
 }
